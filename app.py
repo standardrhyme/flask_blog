fe7cee21fa7769faad63b63901570e59a3e7d860
standardrhyme <asherkang.s@gmail.com>
2023-07-05 21:21:15 -0400

    database connection in app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+
+import sqlite3
+from flask import Flask, render_template
+
+
+def get_db_connection():
+    conn = sqlite3.connection('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
