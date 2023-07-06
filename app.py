585f645be24a72c4722c51dd9e0f93c5e75aae26
standardrhyme <asherkang.s@gmail.com>
2023-07-05 21:39:42 -0400

    grab id numbers for blog posts

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 
 import sqlite3
 from flask import Flask, render_template
-
+from werkzeug.exceptions import abort
 
 def get_db_connection():
-    conn = sqlite3.connection('database.db')
+    conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchnone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 app = Flask(__name__)
 app.debug = True
